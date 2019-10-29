d336c09c35b95db8cea0335f27ba508fdb315475
udwee <jonttu.92@hotmail.com>
2019-10-29 10:23:17 +0200

    RoutingModule initialized and calculator component initialized

--- FrontEndCourse/src/app/app-routing.module.ts
+++ FrontEndCourse/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CalculatorComponent} from "./calculator/calculator.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/calculator', pathMatch: 'full'},
+  {path: 'calculator', component: CalculatorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
