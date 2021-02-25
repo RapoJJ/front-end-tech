e394d5334895e9c22ed821c8c1e3c45193c77612
RapoJJ <Joonas.rapo@gmail.com>
2021-02-25 10:48:50 +0200

    Updated node.js

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCcfY-cDmda22bH7Fg3jZMZLGeOBIsKHFY',
+    authDomain: 'frontendtask-98f6d.firebaseapp.com',
+    databaseURL: 'https://frontendtask-98f6d.firebaseio.com',
+    projectId: 'frontendtask-98f6d',
+    storageBucket: 'frontendtask-98f6d.appspot.com',
+    messagingSenderId: '93876356648',
+  }
 };
