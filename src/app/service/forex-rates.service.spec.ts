258ec730b7f1aface344012cbab39397f4cf8951
RapoJJ <Joonas.rapo@gmail.com>
2021-02-25 16:07:32 +0200

    Update forex-rates.service.spec.ts
    Removed premade test

--- src/app/service/forex-rates.service.spec.ts
+++ src/app/service/forex-rates.service.spec.ts
@@ -4,9 +4,4 @@ import { ForexRatesService } from './forex-rates.service';
 
 describe('ForexRatesService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: ForexRatesService = TestBed.get(ForexRatesService);
-    expect(service).toBeTruthy();
-  });
 });
